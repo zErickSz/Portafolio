bce60719f00a553bea225b73c72e55827fa83dce
unknown <erick-sua@hotmail.com>
2025-06-22 15:21:13 -0500

    Finished

---
+++ assets/js/fadein.js
@@ -0,0 +1,14 @@
+  document.addEventListener("DOMContentLoaded", function () {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+
+    document.querySelectorAll('.fade-in-on-scroll').forEach(img => {
+      observer.observe(img);
+    });
+  });
